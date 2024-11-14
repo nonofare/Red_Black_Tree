fcd8604c6c937e071efbecc805518c8abc48c498
nonofare <adik96885@gmail.com>
2024-11-14 01:51:31 +0100

    Add base class structure

--- Red_Black_Tree/RBT.h
+++ Red_Black_Tree/RBT.h
@@ -3,5 +3,42 @@ namespace RBT {
 	template <typename T>
 	class RedBlackTree {
 
+		struct Node {
+			T data;
+			size_t index;
+			bool is_red;
+			Node* parent;
+			Node* left;
+			Node* right;
+
+			Node(T inData, size_t inIndex) {
+				data = inData;
+				index = inIndex;
+				is_red = false;
+				parent = nullptr;
+				left = nullptr;
+				right = nullptr;
+			}
+
+			~Node() {
+
+			}
+		};
+
+		size_t size;
+		size_t next_index;
+		Node* root;
+
+	public:
+
+		RedBlackTree() {
+			size = 0;
+			next_index = 0;
+			root = nullptr;
+		}
+
+		~RedBlackTree() {
+			//RemoveAllUnder(root);
+		}
 	};
 }
